fae889e2e640bde08e940b9517a0f0b1acbd85b1
Frederik Ruelens <frederik.ruelens@gmail.com>
2018-03-12 10:14:23 +0100

    key-chords

---
+++ init.el
@@ -0,0 +1,133 @@
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
+
+;; Increase gc-cons-threshold, depending on your system you may set it back to a
+;; lower value in your dotfile (function `dotspacemacs/user-config')
+
+(dotimes (n 10)
+  (global-unset-key (kbd (format "M-%d" n))))
+  
+(setq gc-cons-threshold 100000000)
+
+(defconst spacemacs-version         "0.200.13" "Spacemacs version.")
+(defconst spacemacs-emacs-min-version   "24.4" "Minimal version of Emacs.")
+
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (error (concat "Your version of Emacs (%s) is too old. "
+                   "Spacemacs requires Emacs version %s or above.")
+           emacs-version spacemacs-emacs-min-version)
+  (load-file (concat (file-name-directory load-file-name)
+                     "core/core-load-paths.el"))
+  (require 'core-spacemacs)
+  (spacemacs/init)
+  (configuration-layer/sync)
+  (spacemacs-buffer/display-startup-note)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
+
+
+
+
+;; init.el --- Emacs configuration
+
+;; INSTALL PACKAGES
+;; --------------------------------------
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    ein
+    elpy
+    flycheck
+    material-theme
+    key-chord
+    ))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+
+;; PYTHON CONFIGURATION
+;; --------------------------------------
+
+(elpy-enable)
+
+;; use flycheck not flymake with elpy
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; enable autopep8 formatting on save
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(setq py-autopep8-options '("--max-line-length=120"))
+
+
+;; --------------------
+;; Key Chords
+;;---------------------
+(require 'key-chord)
+(key-chord-mode 1)
+
+;; Max time delay between two key presses to be considered a key chord
+(setq key-chord-two-keys-delay 0.1) ; default 0.1
+;; Max time delay between two presses of the same key to be considered a key chord.
+;; Should normally be a little longer than `key-chord-two-keys-delay'.
+(setq key-chord-one-key-delay 0.2) ; default 0.2
+
+(key-chord-define-global "jk" 'undo)
+(key-chord-define-global " b" 'split-window-below)
+(key-chord-define-global " r" 'split-window-right)
+(key-chord-define-global "qq" 'er/expand-region)
+(key-chord-define-global "hh" 'mark-whole-buffer)
+(key-chord-define-global "yy" 'mc/edit-lines)
+(key-chord-define-global "jj" 'helm-M-x)
+
+
+;; Below list is based off that after removing all the key-chord duplicates
+;; like `xs' and `sx'.
+
+;;      fb
+;;      gb gp
+;;  jj  jc jf jg jh jk jl jm jp jq js jt jv jw jx jy jz
+;;  kk
+;;  qq  qb qf qg qh qk ql qm qp qt qv qw qx qy qz
+;;  vv  vc vf vg vh vk vm vp vw vz
+;;  ww
+;;      xb xd xg xk xm xs xw
+;;  yy
+;;      zb zd zf zg zk zm zp zs zw zx
+
+;----------------------
+
+;; init.el ends here
+
+
