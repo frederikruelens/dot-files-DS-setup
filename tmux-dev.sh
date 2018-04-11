9c3e8f6959324a4972464df89655891f3e815e58
Frederik Ruelens <frederik.ruelens@gmail.com>
2018-04-11 12:28:23 +0200

    Add files via upload

---
+++ tmux-dev.sh
@@ -0,0 +1,6 @@
+tmux new-session -s frederik -d
+tmux new-session -s python -d
+tmux new-session -s git -d
+tmux new-session -s dropbox -d
+tmux new-session -s ssh -d
+tmux new-session -s project -d
